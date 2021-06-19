56cb3b79202686411c22a286cb47143598daca98
Jordan Thomas <tjordan706@gmail.com>
2021-06-19 04:04:17 -0400

    complete assignement

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+//attempt #1
 
+function superbowlWin(record) {
+  const result = record.find((record) => record.result === 'W');
+  {
+    if (result === 'W') {
+      console.log(record.year);
+    }
+  }
+}
+
+//attempt #2
+
+function superbowlWin(record) {
+  for (record of collection) {
+    if (record.result === 'W') {
+      console.log(record.year);
+    }
+  }
+}
+superbowlWin(record);
+
+function superbowlWin(record) {
+  const result = record.find((record) => record.result === 'W');
+  return !!result ? result.year : undefined;
+}
